81be8e84742cb16388845ad38e167ec290c656c3
Valeria <v.msalazar@alumnos.upm.es>
2023-04-04 20:27:56 +0200

    Clase pieza

--- tema4inicial_VS19x64/src/Pieza.cpp
+++ tema4inicial_VS19x64/src/Pieza.cpp
@@ -1,1 +1,51 @@
 #include "Pieza.h"
+
+Pieza::Pieza()
+{
+	x = y = 0;
+	rojo = verde = azul = 1;
+	ocupada = false;
+	next = false;
+	tipo = 0;
+
+}
+
+
+Pieza::Pieza(int _x, int _y, bool blanco)
+{
+	x = _x;
+	y = _y;
+	tipo = 0;
+	if (blanco == 0)
+	{
+		rojo = verde = azul = 0;
+
+	}
+	else {
+		rojo = verde = azul = 255;
+
+	}
+
+}
+
+void Pieza::Dibuja()
+{
+
+}
+
+
+Pieza(int _x, int _y, int _tipo, bool color);
+virtual ~Pieza();
+void Dibuja();
+
+void setPosicion(float ix, float iy);
+void setColor(unsigned char r, unsigned char v, unsigned char a);
+void toggleOcupado(); //Cambia si la casilla del tablero está ocupada o no.
+void setNext();
+
+int getPosicion();
+bool getOcupado();
+bool getNext();
+
+
+
--- tema4inicial_VS19x64/src/Pieza.h
+++ tema4inicial_VS19x64/src/Pieza.h
@@ -1,5 +1,43 @@
 #pragma once
+
+
 class Pieza
 {
+private:
+	//Posición
+	float x;
+	float y;
+	//Movimientos
+	bool next;
+	bool ocupada;
+	//Colores
+	unsigned char rojo;
+	unsigned char verde;
+	unsigned char azul;
+
+	int tipo;
+public:
+	Pieza();
+	Pieza(int _x, int _y, bool blanco);//Constructor de peones
+	Pieza(int _x, int _y, int _tipo, bool blanco);
+	virtual ~Pieza();
+	void Dibuja();
+
+	//set
+	void setPosicion(float ix, float iy);
+	void setColor(unsigned char r, unsigned char v, unsigned char a);
+	void toggleOcupado(); //Cambia si la casilla del tablero está ocupada o no.
+	void setNext();
+	void setColor();
+
+
+	//
+	int getColor();
+	int getPosicion();
+	bool getOcupado();
+	bool getNext();
+
+
+
 };
 
