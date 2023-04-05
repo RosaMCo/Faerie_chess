a2e0bd8f650e9ad1765d2d4220bc0c9aff61d317
Valeria <v.msalazar@alumnos.upm.es>
2023-04-05 21:08:38 +0200

    Cambios generales del rey

--- tema4inicial_VS19x64/src/Rey.cpp
+++ tema4inicial_VS19x64/src/Rey.cpp
@@ -1,1 +1,21 @@
 #include "Rey.h"
+#include"freeglut.h"
+Rey::Rey(bool color, Vector2D m)
+{
+
+ }
+
+//Rey ::~Rey()
+//{
+
+//}
+void Rey:: Dibuja()
+{
+
+}
+
+bool Rey::mVal(int dx, int dy, bool color)
+{
+	
+	return true;
+}
--- tema4inicial_VS19x64/src/Rey.h
+++ tema4inicial_VS19x64/src/Rey.h
@@ -1,7 +1,17 @@
 #pragma once
 #include "Pieza.h"
-class Rey
+class Rey:public Pieza
 {
+public:
+
+	Rey(bool color, Vector2D m);
+	//virtual ~Rey();
+
+	//Métodos públicos
+	void Dibuja();
+	bool mVal(int dx, int dy, bool color);
+	
+	
 
 };
 
