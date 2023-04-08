3dd08f1af49e7d65ef99fcf7b1535bafc17af0c8
Valeria <v.msalazar@alumnos.upm.es>
2023-04-08 15:00:38 +0200

    Se han añadido los movimientos del rey y los colores

--- tema4inicial_VS19x64/src/Rey.cpp
+++ tema4inicial_VS19x64/src/Rey.cpp
@@ -2,7 +2,8 @@
 #include"freeglut.h"
 Rey::Rey(bool color, Vector2D m)
 {
-
+	Color = color;
+	posicion = m;
  }
 
 //Rey ::~Rey()
@@ -11,11 +12,53 @@ Rey::Rey(bool color, Vector2D m)
 //}
 void Rey:: Dibuja()
 {
-
+	if (Color== blanco)
+		//Imagen de pieza blanca
+		glEnable(GL_TEXTURE_2D); //Falta modificar para poner la imagen
+	else
+		//Imagen de pieza negra
+		glEnable(GL_TEXTURE_2D);//Falta modificar para poner la imagen del rey negro
 }
 
-bool Rey::mVal(int dx, int dy, bool color)
+bool Rey::mRey(float nx, float ny)
 {
-	
-	return true;
+	//El rey se mueve uno para arriba
+	if (ny == getPosicionY() + 1)
+		//El rey se mueve uno para arriba y uno a la izq, es decir,
+		//diagonal
+		if (nx == getPosicionX() - 1)
+			return true;
+		else if (nx == getPosicionX() + 1)//Diagonal derecha
+			return true;
+		else if (nx == getPosicionX())//No hay mov izq o der
+			return true;
+		else
+			return false;
+
+	//Lo mismo que la anterior pero hacia abajo
+	else if (ny == getPosicionY() - 1)
+		//El rey se mueve uno para arriba y uno a la izq, es decir,
+		//diagonal
+		if (nx == getPosicionX() - 1)
+			return true;
+		else if (nx == getPosicionX() + 1)//Diagonal derecha
+			return true;
+		else if (nx == getPosicionX())//No hay mov izq o der
+			return true;
+		else
+			return false;
+
+	//Se queda en la misma posición
+	else if (ny == getPosicionY())
+		//Mov a la izquierda
+		if (nx == getPosicionX() - 1)
+			return true;
+		//Mov a la derecha
+		else if (nx == getPosicionX() + 1)
+			return true;
+		else
+			return false;
+
+
+
 }
