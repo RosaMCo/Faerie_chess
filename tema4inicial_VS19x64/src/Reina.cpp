9e8936c2bf5430bd06f0c957ea5029270908967e
Valeria <v.msalazar@alumnos.upm.es>
2023-04-15 18:13:43 +0200

    Movimiento de la reina actualizado

--- tema4inicial_VS19x64/src/Reina.cpp
+++ tema4inicial_VS19x64/src/Reina.cpp
@@ -22,10 +22,28 @@ void Reina:: Dibuja()
 bool Reina::mReina(float nx, float ny)
 {
 	//La reina utiliza los movimientos de la torre y del alfil 
-	//Para la torre
+	//Para la torre: La reina debe moverse hacia la derecha "O" a la izquierda:
+	if ((nx == fila) && (ny == columna))
+		return false; //No se puede mover distinta fila y columna
+	else if ((nx == fila) || (ny == columna))
+		return true;//Se puede mover de arriba a abajo o de derecha a izquierda
+
+	//Para el alfil:: La reina se mueve en diagonales
+	//Lo dividimos en dos diagonales como una "x"
+		//Siel destino de la pieza es
+	else if (((nx < fila) && (ny < columna)) || ((nx > fila) && (ny > columna)))//Describí el lugar del movimiento del alfil. 
+		if (((ny - nx) == (columna - fila))|| ((nx + ny) == (columna + fila)))//Con este moimiento bajan o suben dependiendo de la posición del alfil
+			return true;
+		else
+			return false; 
+		//Si el destino de la pieza es
+	else if (((nx < fila) && (ny > columna)) || ((nx > fila) && (ny < columna))) //Descripción del destino de los alfiles  
+		if (((nx + ny) == (columna + fila))|| ((nx + ny) == (columna + fila))) //movimiento de subida o bajada dependiendo de la posición del alfil.
+			return true;
+		else
+			return false;
+	//Como se puede observar, las posiciones de las columnas y filas deben ser iguales para que cumpla la función del movimiento alfil en la reina
+	else
+		return false;
 	
-
-	//Para el alfil
-
-	return true;
 }
