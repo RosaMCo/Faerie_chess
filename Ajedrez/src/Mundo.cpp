497b30bf8ebe27c2d071656d7eb2e0ec3074ad42
Valeria <v.msalazar@alumnos.upm.es>
2023-04-25 00:11:35 +0200

    Introduccion a la creación del menú mundo

--- Ajedrez/src/Mundo.cpp
+++ Ajedrez/src/Mundo.cpp
@@ -1,33 +1,50 @@
 #include "Mundo.h"
-#include "freeglut.h"
-#include <math.h>
 
-void Mundo::rotarOjo()
+//Estado inicial del mundo
+ 
+Mundo::Mundo()
+{
+	estado = Inicio;
+
+}
+/*void Mundo::rotarOjo()
 {
 	float dist=sqrt(x_ojo*x_ojo+z_ojo*z_ojo);
 	float ang=atan2(z_ojo,x_ojo);
 	ang+=0.05f;
 	x_ojo=dist*cos(ang);
 	z_ojo=dist*sin(ang);
-}
+}*/
 void Mundo::dibuja()
 {
-	gluLookAt(x_ojo, y_ojo, z_ojo,  // posicion del ojo
-			0.0, 0, 0.0,      // hacia que punto mira  (0,0,0) 
-			0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
+	if (estado == Inicio) {											//Dibuja la pantalla de inicio
+		gluLookAt(0, 7.5, 30,
+			0.0, 7.5, 0.0,
+			0.0, 1.0, 0.0);
+		//Añadir fondo
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("bin/imagenes/fondo2.png").id);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POLYGON);
+		glColor3f(1, 1, 1);
+		glTexCoord2d(0, 1); glVertex2f(-10, 0);
+		glTexCoord2d(1, 1); glVertex2f(10, 0);
+		glTexCoord2d(1, 0); glVertex2f(10, 15);
+		glTexCoord2d(0, 0); glVertex2f(-10, 15);
+		glEnd();
+		glEnable(GL_LIGHTING);
+		glDisable(GL_TEXTURE_2D);
 
-	//aqui es donde hay que poner el codigo de dibujo
-	//dibujo del suelo
-	glDisable(GL_LIGHTING);
-	glBegin(GL_POLYGON);
-		glColor3ub(255,0,0);
-		glVertex3f(-5.0f,0,-5.0f);
-		glVertex3f(-5.0f,0,5.0f);
-		glColor3ub(255,255,0);
-		glVertex3f(5.0f,0,5.0f);	
-		glVertex3f(5.0f,0,-5.0f);
-	glEnd();
-	glEnable(GL_LIGHTING);
+		ETSIDI::setTextColor(1, 1, 1);
+		ETSIDI::setFont("fuentes/fuente2.otf", 16);
+		ETSIDI::printxy("Ajedrez", -5, 8);
+		ETSIDI::setTextColor(1, 1, 1);
+		ETSIDI::setFont("fuentes/fuente2.otf", 12);
+		ETSIDI::printxy("PULSE LA TECLA -E- PARA EMPEZAR", -5, 7);
+		ETSIDI::printxy("PULSE LA TECLA -S- PARA SALIR", -5, 6);
+		ETSIDI::printxy("PULSE LA TECLA -L- PARA CARGAR LA ULTIMA PARTIDA GUARDADA", -5, 5);
+	}
+	
 }
 
 void Mundo::mueve()
--- Ajedrez/src/Mundo.h
+++ Ajedrez/src/Mundo.h
@@ -1,14 +1,24 @@
-
+#pragma once
+#include "freeglut.h"
+#include <math.h>
+#pragma once
+#include "ETSIDI.h"
 class Mundo
 {
 public: 
+
+	Mundo();
 	void tecla(unsigned char key);
 	void inicializa();
-	void rotarOjo();
+	//void rotarOjo();
 	void mueve();
+	
 	void dibuja();
 
 	float x_ojo;
 	float y_ojo;
 	float z_ojo;
+	//Estados de la máquina:
+	enum ESTADO {Inicio, tienda, juego };
+	ESTADO estado;
 };
