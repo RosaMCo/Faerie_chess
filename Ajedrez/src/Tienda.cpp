56984fc655c546a10ec3b29906fc29f4d0f527a5
Valeria <v.msalazar@alumnos.upm.es>
2023-04-28 21:43:51 +0200

    Modificaciones en tienda

--- Ajedrez/src/Tienda.cpp
+++ Ajedrez/src/Tienda.cpp
@@ -1,6 +1,40 @@
 #include "Tienda.h"
 
+Tienda::Tienda()
+{
+	dinero_inicial = 1000;
+	numero_piezas_max = 16;
+
+}
+void Tienda::inicializa()
+{
+
+}
+
 void Tienda::dibuja()
+{
+	gluLookAt(0, 7.5, 30, // posicion del ojo
+		0.0, 7.5, 0.0, // hacia que punto mira (0,7.5,0) 
+		0.0, 1.0, 0.0); // definimos hacia arriba (eje Y) 
+
+	//Añadir fondo
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("bin/imagenes/fondo4.png").id);
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glColor3f(1, 1, 1);
+	glTexCoord2d(0, 1); glVertex2f(-10, 0);
+	glTexCoord2d(1, 1); glVertex2f(10, 0);
+	glTexCoord2d(1, 0); glVertex2f(10, 15);
+	glTexCoord2d(0, 0); glVertex2f(-10, 15);
+	glEnd();
+	glEnable(GL_LIGHTING);
+	glDisable(GL_TEXTURE_2D);
+
+	
+}
+
+void Tienda::compra()
 {
 
 }
--- Ajedrez/src/Tienda.h
+++ Ajedrez/src/Tienda.h
@@ -1,8 +1,22 @@
 #pragma once
+#include "ETSIDI.h"
+#include "freeglut.h"
 class Tienda
 {
+private:
+
 public:
+
+	int dinero_inicial;
+	int costo;
+	int numero_piezas_max;
+	int numero_piezas;
+
+	Tienda();
+	virtual ~Tienda() {};
 	void dibuja();
+	void compra();
+	void inicializa();
 
 };
 
