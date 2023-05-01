39285681dd7d75a51fcab950cbebe94005c29eee
Valeria <v.msalazar@alumnos.upm.es>
2023-05-01 21:35:35 +0200

    Iluminación de las casillas

--- Ajedrez/src/Casilla.cpp
+++ Ajedrez/src/Casilla.cpp
@@ -1,1 +1,46 @@
 #include "Casilla.h"
+
+
+void Casilla::setPieza(Pieza* pieza)
+{
+	//Esta función se utiliza para poner la pieza.
+	this->pieza = pieza;
+}
+
+Pieza* Casilla::getPieza()
+{
+	//Cogemos una pieza de la casilla y retorna el valor de pieza
+	return pieza;
+}
+
+
+void Casilla::dibuja() {
+
+	glDisable(GL_LIGHTING);
+	glColor3ub(rojo, verde, azul);
+	glBegin(GL_POLYGON);
+	//Posición de la pieza en cada fila y cada columna + un poco de la pieza para recalcar el área
+	glVertex2f(pieza->getFila() + 1.0, pieza->getColumna() + 1.0);
+	glVertex2f(pieza->getFila() + 1.0, pieza->getColumna() - 1.0);
+	glVertex2f(pieza->getFila() - 1.0, pieza->getColumna() - 1.0);
+	glVertex2f(pieza->getFila() - 1.0, pieza->getColumna() + 1.0);
+	glEnd();
+	glEnable(GL_LIGHTING);
+}
+
+
+void Casilla:: Iluminati()	
+{
+	//Se ilumina la la casilla en color rojo
+	rojo = 255;
+	azul = 0;
+	verde = 0;
+}
+
+
+void Casilla::setColor(unsigned char r, unsigned char v, unsigned char a) { //pone el color
+	rojo = r;
+	verde = v;
+	azul = a;
+}
+
--- Ajedrez/src/Casilla.h
+++ Ajedrez/src/Casilla.h
@@ -1,6 +1,31 @@
 #pragma once
-class Casilla
-{
+//EN cada una de las casillas se encuantran las piezas
+#include "Pieza.h"
+#include "freeglut.h"
+
+class Casilla {
+
+	//Atributos privados
+private:
+
+	//Cada casilla debe apuntar a una pieza
+	Pieza* pieza;
+	//Colores
+	unsigned char rojo, verde, azul;
+public:
+
+	
+	Casilla() {};
+	virtual ~Casilla() {};
+
+	void setPieza(Pieza* pieza);
+	Pieza* getPieza();
+	
+	void dibuja();
+	void Iluminati();
+	void setColor(unsigned char r, unsigned char v, unsigned char a);
+
+
+
 
 };
-
