793d3c7454bad76b693a02ed9bf6b730c7c0717b
Valeria <v.msalazar@alumnos.upm.es>
2023-05-07 22:44:42 +0200

    dibujo del rey

--- Ajedrez/src/Rey.cpp
+++ Ajedrez/src/Rey.cpp
@@ -14,12 +14,33 @@ Rey::Rey(Color col, int f, int c)
 //}
 void Rey:: Dibuja()
 {
-	if (color== 1)
+	if (color == blanco)
+	{
 		//Imagen de pieza blanca
-		glEnable(GL_TEXTURE_2D); //Falta modificar para poner la imagen
-	else
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/Fichas/W-King.png").id);
+		glDisable(GL_LIGHTING);
+	}
+	else if (color == negro)
+	{
 		//Imagen de pieza negra
-		glEnable(GL_TEXTURE_2D);//Falta modificar para poner la imagen del rey negro
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/Fichas/B-King.png").id);
+		glDisable(GL_LIGHTING);
+	}
+	//Tengo que hacer el lugar donde se va a situar cada pieza, para ello hago un polígono
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBegin(GL_POLYGON);
+	glColor3f(1, 1, 1);
+	glTexCoord2d(0, 1); glVertex3f(posicion.getX(), posicion.getY(), 0.01);
+	glTexCoord2d(1, 1); glVertex3f(posicion.getX(), posicion.getY(), 0.01);
+	glTexCoord2d(1, 0); glVertex3f(posicion.getX(), posicion.getY(), 0.01);
+	glTexCoord2d(0, 0); glVertex3f(posicion.getX(), posicion.getY(), 0.01);
+	glEnd();
+	glDisable(GL_BLEND);
+	glEnable(GL_LIGHTING);
+	glDisable(GL_TEXTURE_2D);
 }
 
 bool Rey::mRey(float nf, float nc)
