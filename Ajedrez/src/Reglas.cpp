b028378bd3aef4506da6e26281ff27d510fbf766
Valeria <v.msalazar@alumnos.upm.es>
2023-05-20 22:42:45 +0200

    Reglas

---
+++ Ajedrez/src/Reglas.cpp
@@ -0,0 +1,32 @@
+#include "Reglas.h"
+
+int Reglas::Inicializa()
+{
+    unsigned char bufer[TAMANIO_BUFER];
+    FILE* archivo;
+    const char* ReglasAjedrez = "bin/ReglasAjedrez.txt";
+    size_t bytesLeidos;
+
+    archivo = fopen(ReglasAjedrez, "r"); // Abrir en modo read
+    // Si es NULL, entonces no existe, o no se pudo abrir
+    if (!archivo) {
+        printf("¡No se pudo abrir el archivo %s!", ReglasAjedrez);
+        
+    }
+
+    // Mientras no alcancemos el EndOfLine del archivo...
+   while (!feof(archivo)) {
+        // Leer dentro del búfer; fread regresa el número de bytes leídos
+      bytesLeidos = fread(bufer, sizeof(char), sizeof(bufer), archivo);
+        // Para depurar, se imprime la cantidad de bytes leídos (se puede quitar esa parte)
+        //printf("\nSe han leido %lu bytes:\n", bytesLeidos);
+        // Y el contenido del búfer también es impreso
+        fwrite(bufer, sizeof(char), bytesLeidos, stdout);
+      
+   }
+    // Al final, se cierra el archivo
+   
+   exit(0);
+    
+    
+}
---
+++ Ajedrez/src/Reglas.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>// Todas las funciones como fread, fwrite, fopen, fclose y printf
+#include <stdlib.h>// EXIT_FAILURE y EXIT_SUCCESS
+#define TAMANIO_BUFER 16 // Cuántos bytes leer en cada iteración
+using namespace std;
+
+
+class Reglas
+{
+public:
+	
+	int Inicializa();
+};
+
