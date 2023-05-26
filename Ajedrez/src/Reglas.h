5f7f396f281cd7a046917800324a45c10b81366e
Valeria <v.msalazar@alumnos.upm.es>
2023-05-26 21:08:17 +0200

    Se realiza una máquina de estados para sus sencillo funcionamiento, esto incluye un enum para los estados y el propio constructor para inicializar la clase Reglas

--- Ajedrez/src/Reglas.h
+++ Ajedrez/src/Reglas.h
@@ -6,15 +6,17 @@
 using namespace std;
 #define TAMANIO_BUFER 16 // Cuántos bytes leer en cada iteración
 using namespace std;
-
+enum estadoreglas{LEO, NO_LEO};
 
 class Reglas
 {
+	estadoreglas reglas;
 public:
-	
+	Reglas();
 	virtual ~Reglas() {};
 	char opcion;
 	void Inicializa();
+	void tecla(unsigned char key);
 	
 };
 
