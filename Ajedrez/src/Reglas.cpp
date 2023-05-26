bbd2bf88a4b99fad369aefa32cbc2772255967a6
Valeria <v.msalazar@alumnos.upm.es>
2023-05-26 21:08:54 +0200

    Modificación de entrada y salida para que se puedan leer las reglas y que con una tecla te devuelva al estado de inicio

--- Ajedrez/src/Reglas.cpp
+++ Ajedrez/src/Reglas.cpp
@@ -1,7 +1,12 @@
 #include "Reglas.h"
 
+Reglas::Reglas()
+{
+	reglas == NO_LEO;
+}
 void Reglas::Inicializa()
 {
+	reglas == LEO;
     unsigned char bufer[TAMANIO_BUFER];
     FILE* archivo;
     const char* ReglasAjedrez = "bin/ReglasAjedrez.txt";
@@ -26,19 +31,39 @@ void Reglas::Inicializa()
    }
     // Al final, se cierra el archivo
    system("pause");
-pregunta1: 
-   cout << "Desea salir? Pulse Y/N: ";
-   cin >> opcion;
-   switch (opcion)
-   {
-   case 'Y':exit(0) ; break;
-   case'y':exit(0); break;
-   case 'N': system("pause");  goto pregunta1; break;
-   case'n':system("pause"); goto pregunta1; break;
-   default: goto pregunta1; break;
-      
-   }
-       
 
     
+}
+
+void Reglas::tecla(unsigned char key)
+{
+
+/*pregunta1:
+    cout << "Desea salir? Pulse Y/N: ";
+    cin >> key;
+    switch (key)
+    {
+    case 'Y':exit(0); break;
+    case'y':exit(0); break;
+    case 'N': system("pause");  goto pregunta1; break;
+    case'n':system("pause"); goto pregunta1; break;
+    case 'C': 
+    default: goto pregunta1; break;
+
+    }*/
+
+	
+
+	if (reglas == LEO) {
+        switch (key) {
+        pregunta1:
+            cout << "\nPulse cualquier tecla para salir: \n";
+        case 'y':cin >> key; cout << "Volviendo a la pantalla de inicio"; break;
+        
+        default: system("pause"); goto pregunta1; break;
+        }
+
+	}
+    
+	
 }
